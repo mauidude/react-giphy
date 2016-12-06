b2b04126d0a4550b6de0df0d02eaa1a77b498e77
Shane Andrade <shane.andrade@sendgrid.com>
2016-12-06 15:50:39 -0800

    add root react component, hook up to DOM

--- js/index.js
+++ js/index.js
@@ -1,1 +1,13 @@
-console.log('TODO: edit index.jsx');
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+
+export default class ReactGiphy extends Component {
+  render() {
+    return (<h1>Hello, world</h1>);
+  }
+};
+
+ReactDOM.render(
+  <ReactGiphy />,
+  document.getElementById('root')
+);
