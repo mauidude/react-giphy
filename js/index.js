659c79bda20767d39e2cd1a35d7967220fcfd0aa
Shane Andrade <shane.andrade@sendgrid.com>
2016-12-07 09:55:09 -0800

    wire up react and redux

--- js/index.js
+++ js/index.js
@@ -1,9 +1,19 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import {createStore} from 'redux';
+import {Provider} from 'react-redux';
+import app from './reducers';
+import App from './containers/App';
+
+let store = createStore(app);
 
 export default class ReactGiphy extends Component {
   render() {
-    return (<h1>Hello, world</h1>);
+    return (
+      <Provider store={store}>
+        <App />
+      </Provider>
+    );
   }
 };
 
